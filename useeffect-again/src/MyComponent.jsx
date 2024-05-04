9d6393f34ce9d09ec88f48b838006cb7d3c86ada
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-04 23:28:13 +0530

    idk wtf happend I can't run it on web:((((

---
+++ useeffect-again/src/MyComponent.jsx
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+/* useEffect() = React hook that tells React 
+                 TO DO SOME CODE when: (pick one) 
+                 the component re-renders
+                 the component mounts
+                 the state of value changes 
+syntax 
+useEffect(function, [dependencies]);
+useEffect(() => {}) ===>>> runs  after re-render
+useEffect(() => {}, [])  ===>>> runs only when the 
+                                component mounts
+useEffect(() => {}, [value])  ===>>> runs on mount + when 
+                                     value changes */
+
+
+ 
+function MyComponent() {
+
+    const [count, setCount] = useEffect(0);
+
+    function addCount() {
+        setCount(c => c + 1);
+    }
+
+  return(
+        <div>
+            <p> Count: {count} </p>
+            <button onClick={addCount} > add </button>
+        </div>
+  );
+}
+export default MyComponent;
+
+
+
+
+
+
